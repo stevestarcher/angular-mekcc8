41aa8ad498aac13f0155c973a60ce1f3cca4ba44
stevestarcher <stevestarcher@hotmail.com>
2022-02-10 20:54:59 -0600

    2 files modified, 2 added

---
+++ src/app/product-details/product-details.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { Product, products } from '../products';
+export class ProductDetailsComponent implements OnInit {
+
+  product: Product | undefined;
+  /* ... */
+}
+export class ProductDetailsComponent implements OnInit {
+
+  product: Product | undefined;
+
+  constructor(private route: ActivatedRoute) { }
+
+}
+ngOnInit() {
+  // First get the product id from the current route.
+  const routeParams = this.route.snapshot.paramMap;
+  const productIdFromRoute = Number(routeParams.get('productId'));
+
+  // Find the product that correspond with the id provided in route.
+  this.product = products.find(product => product.id === productIdFromRoute);
+}
